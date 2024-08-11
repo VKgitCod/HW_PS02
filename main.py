67886311114ef40cf4fcc695a94a17c23fce7b43
VKgitCod <itcreatorvak@gmail.com>
2024-08-11 14:22:00 +0300

    add code

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import pprint
+
+params = {
+    'q' : 'html'
+}
+
+response = requests.get('https://api.github.com/search/repositories', params=params)
+
+response_json = response.json()
+
+print(f'количество репозиториев с использованием html: {response_json['total_count']}')
+
+print(response.status_code)
+
+pprint.pprint(response_json)
+
+
+
