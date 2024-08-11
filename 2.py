f70ab554c8342c9c72d112be3dbd0be5a81b4cfb
VKgitCod <itcreatorvak@gmail.com>
2024-08-11 14:29:16 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {
+        "userId" : 1
+}
+
+response = requests.get(url, data=data)
+
+print(response.status_code)
+
+print(response.headers)
+
+print(response.text)
